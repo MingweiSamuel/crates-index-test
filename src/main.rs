cc21caf82a48563afd2743edd9832e767545cc4b
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-01-26 15:57:28 -0800

    add index update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
-use crates_index::GitIndex;
+use crates_index::{GitIndex, SparseIndex};
 
 fn main() {
-    let index = GitIndex::new_cargo_default().unwrap();
-    let c = index.crate_("hydroflow").unwrap();
-    println!("{:#?}", c.versions().iter().map(|v| v.version()).collect::<Vec<_>>());
+    {
+        let mut index = GitIndex::new_cargo_default().unwrap();
+        println!("URL {}", index.url());
+        index.update().unwrap();
+        let c = index.crate_("gitoxide").unwrap();
+        println!(
+            "{:#?}",
+            c.versions().iter().map(|v| v.version()).collect::<Vec<_>>()
+        );
+    }
+    {
+        let index = SparseIndex::new_cargo_default().unwrap();
+        let c = index.crate_from_cache("gitoxide").unwrap();
+        println!(
+            "{:#?}",
+            c.versions().iter().map(|v| v.version()).collect::<Vec<_>>()
+        );
+    }
 }
